8760446c2b94d9f57005b2d9f1cace85a70c4269
Manoj <114227142+Manoj1408@users.noreply.github.com>
2023-04-24 22:22:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //your JS code here. If required.
 let btn = document.getElementById("enterBtn");
 btn.addEventListener('click',() => {
-  document.getElementById('status').innerText = 'Entered Metaverse'
+  // document.getElementById('status')[0].innerText = 'Entered Metaverse'
+	document.querySelector('#status h1').innerText = 'Entered Metaverse';
 })
