77f1fe6aa7eeb5120168be256cb04a4cec159888
Manoj <114227142+Manoj1408@users.noreply.github.com>
2023-04-24 22:46:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let btn = document.getElementById("enterBtn");
 btn.addEventListener('click',() => {
 	// let ele = document.getElementById('status')
 	// console.log(ele);
-		document.getElementsByTagName('h1')[0].innerText = 'Entered Metaverse';
-  // document.getElementById('status')[0].innerText = 'Entered Metaverse'
+		// document.getElementsByTagName('h1')[0].innerText = 'Entered Metaverse';
+  document.getElementById('status').innerText = 'Entered Metaverse'
 	// document.querySelector('#status h1').innerText = 'Entered Metaverse';
 })
