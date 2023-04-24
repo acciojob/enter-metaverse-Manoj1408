99792c67868c89184638cd4aad91d177bbfdee99
Manoj <114227142+Manoj1408@users.noreply.github.com>
2023-04-24 22:38:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 //your JS code here. If required.
 let btn = document.getElementById("enterBtn");
 btn.addEventListener('click',() => {
+	// let ele = document.getElementById('status')
+	// console.log(ele);
+		document.getElementsByTagName('h1')[0].innerText = 'Entered Metaverse';
   // document.getElementById('status')[0].innerText = 'Entered Metaverse'
-	document.querySelector('#status h1').innerText = 'Entered Metaverse';
+	// document.querySelector('#status h1').innerText = 'Entered Metaverse';
 })
